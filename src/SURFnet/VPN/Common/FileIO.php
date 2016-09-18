4d21b9e846548a40989923a089ea3aa870a79a44
François Kooman <fkooman@tuxed.net>
2016-09-18 20:34:25 +0200

    add FileIO class

---
+++ src/SURFnet/VPN/Common/FileIO.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *  Copyright (C) 2016 SURFnet.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace SURFnet\VPN\Common;
+
+class FileIO
+{
+    public static function readFile($filePath)
+    {
+        if (false === $fileData = @file_get_contents($filePath)) {
+            throw new RuntimeException(sprintf('unable to read file "%s"', $filePath));
+        }
+
+        return $filePath;
+    }
+
+    public static function readJsonFile($filePath)
+    {
+        $fileData = self::readFile($filePath);
+        $jsonData = json_decode($fileData, true);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new RuntimeException(sprintf('unable to decode JSON from file "%s"', $filePath));
+        }
+    }
+
+    public static function writeFile($filePath, $fileData)
+    {
+        if (false === @file_put_contents($filePath, $fileData)) {
+            throw new RuntimeException(sprintf('unable to write file "%s"', $filePath));
+        }
+    }
+
+    public static function writeJsonFile($filePath, $fileJsonData)
+    {
+        $fileData = json_encode($fileJsonData);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new RuntimeException(sprintf('unable to encode JSON for file "%s"', $filePath));
+        }
+
+        self::writeFile($filePath, json_encode($fileJsonData));
+    }
+}
