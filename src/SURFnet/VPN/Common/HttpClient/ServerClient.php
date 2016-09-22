b402c5432a3bba1c7932be88a140947b2c3bd99d
François Kooman <fkooman@tuxed.net>
2016-09-22 11:29:34 +0200

    implement serverPool call

--- src/SURFnet/VPN/Common/HttpClient/ServerClient.php
+++ src/SURFnet/VPN/Common/HttpClient/ServerClient.php
@@ -90,6 +90,11 @@ class ServerClient extends BaseClient
         return $this->post('kill_client', ['common_name' => $commonName]);
     }
 
+    public function serverPool($poolId)
+    {
+        return $this->get('server_pool', ['pool_id' => $poolId]);
+    }
+
     public function serverPools()
     {
         return $this->get('server_pools');
