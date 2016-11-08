38fa0ddf4ce9822a666b987d6f8e6bea09145349
François Kooman <fkooman@tuxed.net>
2016-11-08 14:34:00 +0100

    update dependencies, source formatting

--- src/SURFnet/VPN/Common/HttpClient/BaseClient.php
+++ src/SURFnet/VPN/Common/HttpClient/BaseClient.php
@@ -15,6 +15,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace SURFnet\VPN\Common\HttpClient;
 
 use SURFnet\VPN\Common\HttpClient\Exception\HttpClientException;
