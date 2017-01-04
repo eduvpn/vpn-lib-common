f0f3ad16b8f26c2d17879f4e0202f4e44730062a
François Kooman <fkooman@tuxed.net>
2017-01-04 01:21:16 +0100

    cleanup

--- src/HttpClient/CurlHttpClient.php
+++ src/HttpClient/CurlHttpClient.php
@@ -43,43 +43,35 @@ class CurlHttpClient implements HttpClientInterface
 
     public function get($requestUri)
     {
-        $curlOptions = [
-            CURLOPT_USERPWD => sprintf('%s:%s', $this->authInfo[0], $this->authInfo[1]),
-            CURLOPT_URL => $requestUri,
-            CURLOPT_HEADER => 0,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_FOLLOWLOCATION => 0,
-            CURLOPT_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS,
-        ];
-
-        if (false === curl_setopt_array($this->curlChannel, $curlOptions)) {
-            throw new RuntimeException('unable to set cURL options');
-        }
-
-        if (false === $responseData = curl_exec($this->curlChannel)) {
-            throw new RuntimeException('failure performing the HTTP request');
-        }
-
-        $responseData = json_decode($responseData, true);
-        // XXX check for errors!
-
-        return [
-            curl_getinfo($this->curlChannel, CURLINFO_HTTP_CODE),
-            $responseData,
-        ];
+        return $this->exec(
+            [
+                CURLOPT_URL => $requestUri,
+            ]
+        );
     }
 
     public function post($requestUri, array $postData = [])
     {
-        $curlOptions = [
-            CURLOPT_USERPWD => sprintf('%s:%s', $this->authInfo[0], $this->authInfo[1]),
-            CURLOPT_URL => $requestUri,
-            CURLOPT_HEADER => 0,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_FOLLOWLOCATION => 0,
-            CURLOPT_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS,
-            CURLOPT_POSTFIELDS => http_build_query($postData),
-        ];
+        return $this->exec(
+            [
+                CURLOPT_URL => $requestUri,
+                CURLOPT_POSTFIELDS => http_build_query($postData),
+            ]
+        );
+    }
+
+    private function exec(array $curlOptions)
+    {
+        $curlOptions = array_merge(
+            [
+                CURLOPT_USERPWD => sprintf('%s:%s', $this->authInfo[0], $this->authInfo[1]),
+                CURLOPT_HEADER => 0,
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_FOLLOWLOCATION => 0,
+                CURLOPT_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS,
+            ],
+            $curlOptions
+        );
 
         if (false === curl_setopt_array($this->curlChannel, $curlOptions)) {
             throw new RuntimeException('unable to set cURL options');
@@ -89,12 +81,9 @@ class CurlHttpClient implements HttpClientInterface
             throw new RuntimeException('failure performing the HTTP request');
         }
 
-        $responseData = json_decode($responseData, true);
-        // XXX check for errors!
-
         return [
             curl_getinfo($this->curlChannel, CURLINFO_HTTP_CODE),
-            $responseData,
+            json_decode($responseData, true),
         ];
     }
 }
