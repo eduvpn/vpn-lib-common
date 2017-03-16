cca9396f6635717a25d8d72e800e5ca03923ca46
François Kooman <fkooman@tuxed.net>
2017-03-16 12:47:37 +0100

    move multilang here, as well as template manager

--- .phan/config.php
+++ .phan/config.php
@@ -4,6 +4,8 @@ return [
     'directory_list' => [
         'src',
         'vendor/psr/log',
+        'vendor/twig/twig',
+        'vendor/twig/extensions/',
     ],
     'exclude_analysis_directory_list' => [
         'vendor/',
