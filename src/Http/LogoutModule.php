1733915a999bf7402444fcd365f82f92442a639d
François Kooman <fkooman@tuxed.net>
2018-11-27 21:57:43 +0100

    remain compatible with old "logout" endpoint

--- src/Http/LogoutModule.php
+++ src/Http/LogoutModule.php
@@ -32,36 +32,61 @@ class LogoutModule implements ServiceModuleInterface
     }
 
     /**
+     * @param \SURFnet\VPN\Common\Http\Service $service
+     *
      * @return void
      */
     public function init(Service $service)
     {
+        // new URL since we introduce SAML / Mellon logout
         $service->post(
             '/_logout',
             /**
              * @return \SURFnet\VPN\Common\Http\Response
              */
             function (Request $request, array $hookData) {
-                $this->session->destroy();
-                $httpReferrer = $request->requireHeader('HTTP_REFERER');
-                if ($this->isMellon) {
-                    $mellonLogoutUrl = sprintf('%s/%s', $request->getAuthority(), self::MELLON_LOGOUT);
-
-                    return new RedirectResponse(
-                        sprintf(
-                            '%s?%s',
-                            $mellonLogoutUrl,
-                            http_build_query(
-                                [
-                                    'ReturnTo' => $httpReferrer,
-                                ]
-                            )
-                        )
-                    );
-                }
-
-                return new RedirectResponse($httpReferrer);
+                return $this->doLogout($request);
             }
         );
+
+        // this is an "alias" to remain compatible with the old "base.twig"
+        // template in case it was locally overridden...
+        $service->post(
+            '/_form/auth/logout',
+            /**
+             * @return \SURFnet\VPN\Common\Http\Response
+             */
+            function (Request $request, array $hookData) {
+                return $this->doLogout($request);
+            }
+        );
+    }
+
+    /**
+     * @param \SURFnet\VPN\Common\Http\Request $request
+     *
+     * @return \SURFnet\VPN\Common\Http\Response
+     */
+    private function doLogout(Request $request)
+    {
+        $this->session->destroy();
+        $httpReferrer = $request->requireHeader('HTTP_REFERER');
+        if ($this->isMellon) {
+            $mellonLogoutUrl = sprintf('%s/%s', $request->getAuthority(), self::MELLON_LOGOUT);
+
+            return new RedirectResponse(
+                sprintf(
+                    '%s?%s',
+                    $mellonLogoutUrl,
+                    http_build_query(
+                        [
+                            'ReturnTo' => $httpReferrer,
+                        ]
+                    )
+                )
+            );
+        }
+
+        return new RedirectResponse($httpReferrer);
     }
 }
