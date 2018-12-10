d67eea36820f7e80f71397014885193fc133ecfd
François Kooman <fkooman@tuxed.net>
2018-12-10 19:35:16 +0100

    Revert "add Utils::getInWhitelist helper function"
    This reverts commit 62aca5f0b17cb93a03c7cbafad998806479a2a82.

--- src/Http/Utils.php
+++ src/Http/Utils.php
@@ -14,26 +14,6 @@ use SURFnet\VPN\Common\Http\Exception\HttpException;
 class Utils
 {
     /**
-     * @param Request                     $request
-     * @param array<string,array<string>> $whiteList
-     *
-     * @return bool
-     */
-    public static function getInWhitelist(Request $request, array $whiteList)
-    {
-        $requestMethod = $request->getRequestMethod();
-        if (!array_key_exists($requestMethod, $whiteList)) {
-            return false;
-        }
-
-        if (!\in_array($request->getPathInfo(), $whiteList[$requestMethod], true)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
      * @param array  $sourceData
      * @param string $key
      * @param bool   $isRequired
