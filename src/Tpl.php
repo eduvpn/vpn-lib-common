c76f9ac7a55ed5f0df323cbb3596b5fac970b477
François Kooman <fkooman@tuxed.net>
2018-12-14 16:08:35 +0100

    add callback to Tpl

--- src/Tpl.php
+++ src/Tpl.php
@@ -18,11 +18,12 @@ class Tpl implements TplInterface
 
     /**
      * @param array<string> $templateDirs
-     * @param string        $localeFile
+     * @param null|string   $localeFile
      */
     public function __construct(array $templateDirs, $localeFile)
     {
         $this->tpl = new Template($templateDirs, $localeFile);
+        $this->tpl->addCallback('bytes_to_human', [\get_class($this), 'toHuman']);
     }
 
     /**
@@ -45,5 +46,29 @@ class Tpl implements TplInterface
             $templateName,
             $templateVariables
         );
+    }
+
+    /**
+     * @param int $byteSize
+     *
+     * @return string
+     */
+    public static function toHuman($byteSize)
+    {
+        $kB = 1024;
+        $MB = $kB * 1024;
+        $GB = $MB * 1024;
+        $TB = $GB * 1024;
+        if ($byteSize > $TB) {
+            return sprintf('%0.2f TiB', $byteSize / $TB);
+        }
+        if ($byteSize > $GB) {
+            return sprintf('%0.2f GiB', $byteSize / $GB);
+        }
+        if ($byteSize > $MB) {
+            return sprintf('%0.2f MiB', $byteSize / $MB);
+        }
+
+        return sprintf('%0.0f kiB', $byteSize / $kB);
     }
 }
