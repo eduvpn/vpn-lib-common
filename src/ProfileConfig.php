cbead1be00b69a52300b46e93f517aa259889588
François Kooman <fkooman@tuxed.net>
2020-10-28 22:56:31 +0100

    refactor ProfileConfig

--- src/ProfileConfig.php
+++ src/ProfileConfig.php
@@ -9,37 +9,209 @@
 
 namespace LC\Common;
 
-class ProfileConfig extends Config
+class ProfileConfig
 {
-    public function __construct(array $configData)
+    /** @var Config */
+    private $config;
+
+    public function __construct(Config $config)
     {
-        parent::__construct($configData);
+        $this->config = $config;
+    }
+
+    /**
+     * @return int
+     */
+    public function profileNumber()
+    {
+        return $this->config->requireInt('profileNumber');
+    }
+
+    /**
+     * @return string
+     */
+    public function hostName()
+    {
+        return $this->config->requireString('hostName');
+    }
+
+    /**
+     * @return string
+     */
+    public function range()
+    {
+        return $this->config->requireString('range');
+    }
+
+    /**
+     * @return string
+     */
+    public function range6()
+    {
+        return $this->config->requireString('range6');
+    }
+
+    /**
+     * @return string
+     */
+    public function displayName()
+    {
+        return $this->config->requireString('displayName');
+    }
+
+    /**
+     * @return bool
+     */
+    public function defaultGateway()
+    {
+        return $this->config->requireBool('defaultGateway', false);
     }
 
     /**
      * @return array
      */
-    public static function defaultConfig()
+    public function routes()
     {
-        return [
-            'defaultGateway' => false,
-            'routes' => [],
-            'dns' => [],
-            'dnsSuffix' => [],
-            'clientToClient' => false,
-            'listen' => '::',
-            'enableLog' => false,
-            'enableAcl' => false,
-            'aclPermissionList' => [],
-            'managementIp' => '127.0.0.1',
-            'vpnProtoPorts' => [
-                'udp/1194',
-                'tcp/1194',
-            ],
-            'exposedVpnProtoPorts' => [],
-            'hideProfile' => false,
-            'tlsProtection' => 'tls-crypt',
-            'blockLan' => false,
-        ];
+        return $this->config->requireArray('routes', []);
+    }
+
+    /**
+     * @return array
+     */
+    public function dns()
+    {
+        return $this->config->requireArray('dns', []);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @return array
+     */
+    public function dnsSuffix()
+    {
+        return $this->config->requireArray('dnsSuffix', []);
+    }
+
+    /**
+     * @return bool
+     */
+    public function clientToClient()
+    {
+        return $this->config->requireBool('clientToClient', false);
+    }
+
+    /**
+     * @return string
+     */
+    public function listen()
+    {
+        return $this->config->requireString('listen', '::');
+    }
+
+    /**
+     * @return bool
+     */
+    public function enableLog()
+    {
+        return $this->config->requireBool('enableLog', false);
+    }
+
+    /**
+     * @return bool
+     */
+    public function enableAcl()
+    {
+        return $this->config->requireBool('enableAcl', false);
+    }
+
+    /**
+     * @return array
+     */
+    public function aclPermissionList()
+    {
+        return $this->config->requireArray('aclPermissionList', []);
+    }
+
+    /**
+     * @return string
+     */
+    public function managementIp()
+    {
+        return $this->config->requireString('managementIp', '127.0.0.1');
+    }
+
+    /**
+     * @return array
+     */
+    public function vpnProtoPorts()
+    {
+        return $this->config->requireArray('vpnProtoPorts', []);
+    }
+
+    /**
+     * @return array
+     */
+    public function exposedVpnProtoPorts()
+    {
+        return $this->config->requireArray('exposedVpnProtoPorts', []);
+    }
+
+    /**
+     * @return bool
+     */
+    public function hideProfile()
+    {
+        return $this->config->requireBool('hideProfile', false);
+    }
+
+    /**
+     * @return string
+     */
+    public function tlsProtection()
+    {
+        return $this->config->requireString('tlsProtection', 'tls-crypt');
+    }
+
+    /**
+     * @return bool
+     */
+    public function blockLan()
+    {
+        return $this->config->requireBool('blockLan', false);
+    }
+
+    /**
+     * @return bool
+     */
+    public function tlsOneThree()
+    {
+        return $this->config->requireBool('tlsOneThree', false);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function dnsDomain()
+    {
+        return $this->config->optionalString('dnsDomain');
+    }
+
+    /**
+     * @return array
+     */
+    public function dnsDomainSearch()
+    {
+        return $this->config->requireArray('dnsDomainSearch', []);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->config->toArray();
     }
 }
